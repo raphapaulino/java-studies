35b35bcf12d170b4f0da4928a4a67b68e46a5eed
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-01-08 06:19:21 -0300

    - finished 'Java com JDBC' course

---
+++ Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/App.java
@@ -0,0 +1,36 @@
+package com.schoolofnet.Java_Jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App 
+{
+//    public static void main( String[] args )
+//    {
+//        try {
+//			// Class.forName("com.mysql.jdbc.Driver"); // for backward compatibility
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			System.out.println("Loaded");
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			System.out.println("Failed to Load");
+//			e.printStackTrace();
+//		}
+//        
+//        Connection connection = null;
+//        
+//        try {
+//			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/studies_school_java_movies?serverTimeZone=UTC&useSSL=false&allowPublicKeyRetrieval=true", "raphael", "wiccano28");
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        if (connection != null) {
+//        	System.out.println("Connected");
+//        } else {
+//        	System.out.println("Connection failed");
+//        }
+//    }
+}

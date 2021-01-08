17aad86f28be1589650a0f67cc3103bbd7e49768
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-01-08 06:19:21 -0300

    - finished 'Java com JDBC' course

---
+++ Java_Jdbc/src/main/java/com/schoolofnet/Java_Jdbc/ConnectionFactory.java
@@ -0,0 +1,36 @@
+package com.schoolofnet.Java_Jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	private final String URL = "jdbc:mysql://localhost:3306/studies_school_java_movies?serverTimeZone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+	private final String USER = "raphael";
+	private final String PASSWORD = "***REMOVED***";
+	private final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+	
+	private Connection createConnection() throws ClassNotFoundException {
+		Connection connection = null;
+		
+		try {
+			Class.forName(DRIVER_CLASS);
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+	
+	public Connection getConnection() {
+		try {
+			return createConnection();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
